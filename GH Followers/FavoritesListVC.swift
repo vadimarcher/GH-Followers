af75f10afa1bc3aaf92a4c83e83278c31a03bf20
Vadim Archer <vadim.archer@gmail.com>
2022-08-12 17:28:20 +0300

    TextFiled and Button customisation

---
+++ GH Followers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GH Followers
+//
+//  Created by Vadim Archer on 10.08.2022.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ GH Followers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GH Followers
+//
+//  Created by Vadim Archer on 10.08.2022.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
